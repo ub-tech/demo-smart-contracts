13f0745ecdf88ec501d2c86492fc81a14a743cf2
ilvcs <rammbbs2008@gmail.com>
2022-08-26 22:29:49 +0530

    ERC721 contract with ERC721Enumerable with TokenURI added

--- scripts/deploy_NFTMarketplace.js
+++ scripts/deploy_NFTMarketplace.js
@@ -3,17 +3,20 @@ const hre = require("hardhat");
 /**
  * NFTMinter_Address = '0x89eE595079a3445f1981aaDb830CAfBe5B739f8b'
  * ERC20 contract Address: 0x67c5518B7f3ea8397C9C6e98598B8A737B462289
-   NFTMarketplace contract Address: 0x9189F01E93D85B3b71551d949e1d76e38D868C07
+ * SUSDC_MINT_ON_DEMAND = '0x907Cb90455f47F8E93fCBfEA31C1614A5e49D8B4'
+   OLD NFTMarketplace contract Address: 0x9189F01E93D85B3b71551d949e1d76e38D868C07
+   NEW NFTMarketplace contract Address: 0x59e8AA57Ea44e8C75FB7969c3307b2280379c9A5
  */
 
 const NFTMinter_Address = '0x89eE595079a3445f1981aaDb830CAfBe5B739f8b'
-const SUSDC_Address = '0x67c5518B7f3ea8397C9C6e98598B8A737B462289'
+//const SUSDC_Address = '0x67c5518B7f3ea8397C9C6e98598B8A737B462289'
+const SUSDC_MINT_ON_DEMAND = '0x907Cb90455f47F8E93fCBfEA31C1614A5e49D8B4'
 
 const main = async() => {
   const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
  
-  const nftMarketPlace = await NFTMarketplace.deploy(NFTMinter_Address, SUSDC_Address)
-  const tx = await nftMarketPlace.deployed()
+  const nftMarketPlace = await NFTMarketplace.deploy(NFTMinter_Address, SUSDC_MINT_ON_DEMAND)
+  await nftMarketPlace.deployed()
   console.log(`NFTMarketplace contract Address: ${nftMarketPlace.address}`)
 }
 
